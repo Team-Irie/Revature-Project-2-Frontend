59c8213822344d6309fcca9cfc13121d83e6d855
KHADGA-R <parajuli.kr@gmail.com>
2022-02-23 17:03:39 -0500

    login page layout

---
+++ restaurant-reservation-system/src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthServiceService } from '../auth-service.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  formGroup = FormGroup;
+
+
+  constructor(private authService:AuthServiceService) { }
+
+  ngOnInit()  {
+    this.initForm();
+   }
+
+  initForm(){
+    this.formGroup = new FormGroup({
+      username: new FormControl("",[Validators.required]),
+      password: new FormControl("",[Validators.required])
+    })
+  }
+  loginProcess() {
+    if(this.formGroup.valid) {
+      this.authService.login(this.formGroup.value).subscribe(result => {
+        if(result.success){
+          console.log(result);
+          alert(result.messsage);
+        }else {
+          alert(result.messsage)
+        }
+        
+      });
+    }
+  }
+
+}
