83d0df28f9c4a7cbeb7a32f361355410c10db349
Arby Gudes <aegudes@gmail.com>
2022-02-24 15:51:04 -0600

    moving login to components folder

--- restaurant-reservation-system/src/app/app.module.ts
+++ restaurant-reservation-system/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import {MatFormFieldModule} from '@angular/material/form-field';
