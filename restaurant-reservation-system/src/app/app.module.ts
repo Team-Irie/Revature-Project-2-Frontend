f82b050efdccda55bed3e5405070bf416e3f641d
Arby Gudes <aegudes@gmail.com>
2022-02-24 11:44:43 -0600

    clearing conflicts

--- restaurant-reservation-system/src/app/app.module.ts
+++ restaurant-reservation-system/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AccountInfoPageComponent } from './components/account-info-page/account-info-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CustomerViewReservationsPageComponent } from './components/customer-view-reservations-page/customer-view-reservations-page.component';
+import { ReservationComponent } from './components/reservation/reservation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AccountInfoPageComponent,
-    NavbarComponent
+    NavbarComponent,
+    CustomerViewReservationsPageComponent,
+    ReservationComponent
   ],
   imports: [
     BrowserModule,
