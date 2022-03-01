4eb5e8a6b7e085624fa34f20137438e7362ce5cc
Ryan Sy <ryanbsy@gmail.com>
2022-03-01 17:13:46 -0500

    searchbox is functional

--- restaurant-reservation-system/src/app/components/searchbox/searchbox.component.ts
+++ restaurant-reservation-system/src/app/components/searchbox/searchbox.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
 import {ErrorStateMatcher} from '@angular/material/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
 import { SearchboxService } from 'src/app/services/searchbox.service';
-// import { Restaurant, RestaurantInformation } from 'src/app/Interfaces/IRestaurant';
 
 @Component({
   selector: 'app-searchbox',
@@ -12,43 +10,32 @@ import { SearchboxService } from 'src/app/services/searchbox.service';
 })
 export class SearchboxComponent implements OnInit {
 
+  private term!: string;
+  private location!: string;
+
   restaurants = new Array<any>();
-  // searchYelp: any;
 
   constructor(public searchboxService: SearchboxService) {}
   
   onSubmit(data:any) {
     console.log('form submitted');
-    const term = data.value.term;
-    const location = data.value.location;
-    this.searchboxService.searchYelp().subscribe(response => {
+    let term = data.value.term;
+    let location = data.value.location;
+    this.searchboxService.searchYelp(term, location).subscribe(response => {
+      /*
+      response.business is an array
+      in this case we are showing the first element only, we can easily show more
+      */ 
       this.restaurants = response.businesses;
-      console.log(response.businesses[0].name);
+      console.log('name', response.businesses[0].name);
+      console.log('image_url', response.businesses[0].image_url);
+      console.log('location.display_address[0]', response.businesses[0].location.display_address[0]);
+      console.log('location.display_address[1]', response.businesses[0].location.display_address[1]);
+      console.log('display_phone', response.businesses[0].display_phone);
       return response.businesses;
     });
   }
   
   ngOnInit(): void {}
-  // public onSubmit(data:any) {
-  //   console.log('form submitted');
-
-  //   restaurants = new Array<any>();
-    
-    // for term and location, convert whitespace to +
-    // const term = data.value.term;
-    // const location = data.value.location;
-
-    // let queryParams = new HttpParams();
-    // queryParams = queryParams.append('term', term);
-    // queryParams = queryParams.append('location', location);
-
-    // const url = `https://localhost:7000/search?term=${term}&location=${location}&limit=1`;
-
-    // return this.http.get<RestaurantInformation>(url, {params: queryParams});
-    // console.log('search result:', this.http.get<RestaurantInformation>(url, {params: queryParams}));
-    
-  // }
-
-  
 
 }
--- restaurant-reservation-system/src/app/services/searchbox.service.ts
+++ restaurant-reservation-system/src/app/services/searchbox.service.ts
@@ -7,10 +7,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class SearchboxService {
+  private term!: string;
+  private location!: string; 
+
   constructor(private http: HttpClient) {}
 
-  public searchYelp(): Observable<any> {    
-    const url = 'http://localhost:7000/search?term=edwards&location=07302';
+  public searchYelp(value: string, value2: string): Observable<any> {
+    this.term = value;
+    this.location = value2;    
+    const url = `http://localhost:7000/search?term=${this.term}&location=${this.location}`;
     return this.http.get<any>(url);
   } 
 
