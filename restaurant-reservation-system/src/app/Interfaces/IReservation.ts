a47463df1497c82dc2cf729e44a05c6f72c48623
Arby Gudes <aegudes@gmail.com>
2022-03-04 10:53:28 -0600

    saved changes

--- restaurant-reservation-system/src/app/Interfaces/IReservation.ts
+++ restaurant-reservation-system/src/app/Interfaces/IReservation.ts
@@ -5,10 +5,6 @@ export interface IReservation {
     reservationTime: number;
     restaurantName: string;
     restaurantAddress?: string;
-<<<<<<< HEAD
-    restaurantPhoneNumber?: string;
-=======
     restaurantPhone?: string;
     reservationStatus: string;
->>>>>>> 381d2d6ebc16762056df05f563214015c449e15e
 }
