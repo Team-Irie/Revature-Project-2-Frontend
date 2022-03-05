5fcb91e890ed2edaef099066fe151fc0f6c536c3
Ryan Sy <ryanbsy@gmail.com>
2022-03-05 18:58:59 -0500

    Revert "Login"

--- restaurant-reservation-system/src/app/components/login/login.component.ts
+++ restaurant-reservation-system/src/app/components/login/login.component.ts
@@ -1,6 +1,10 @@
+import { IUser } from '../../Interfaces/IUser';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LoginService } from 'src/app/services/login.service';
+import { UserServiceService } from 'src/app/services/user-service.service';
+
 
 @Component({
   selector: 'app-login',
@@ -11,33 +15,32 @@ export class LoginComponent implements OnInit {
 
   userData = {}
   
-  email:string = '';
-  password:string ='';
-  error: boolean = false;
+  public loginForm !: FormGroup;
+  
+  user:IUser = {
+    userId: 0,
+    firstName:"",
+    lastName: "",
+    email: "",
+    password: "",
+    userType: "",
+    phoneNumber: ""
+  }
 
-  constructor(public loginService: LoginService,  private router:Router) { }
+  hide = true;
 
-  ngOnInit(): void {}
+  constructor(
+    private formBuilder : FormBuilder,
+    private http: HttpClient,
+    private router:Router,
+    private userService:UserServiceService
+    ) { }
 
-  login(): void {
-    
-    this.loginService.loginUser(this.email, this.password)
-      .subscribe(data => {
-        if (data) {
-          this.loginService.user = {
-            userId: data.userId,
-            email: data.email,
-            firstName: data.firstName,
-            lastName: data.lastName,
-            password: data.password,
-            phoneNumber: data.phoneNumber,
-            userType: data.userType
-          }
-          this.error = false;
-          this.router.navigateByUrl('/home');
-        }
-    }, (error) => this.error = true);
-
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username:[''],
+      password:['']
+    });
    }
 /*
    login():void {
@@ -55,5 +58,26 @@ export class LoginComponent implements OnInit {
     )
   }
   */
+
+   login(){
+    this.userService.login(this.loginForm.value.username,this.loginForm.value.password)
+    .subscribe(data =>{
+      console.log(data);
+      /*
+      this.user.userId = data.userId;
+      this.user.firstName = data.firstName;
+      this.user.lastName = data.lastName;
+      this.user.email = data.email;
+      this.user.password = data.password;
+      this.user.userType = data.userType;
+      this.user.phoneNumber = data.phoneNumber;
+      console.log(this.user);*/
+    });
+   }
+
+   testPathCustomer():void {
+    this.router.navigate(['customer-view-reservations'])
+   }
+
 
 }
